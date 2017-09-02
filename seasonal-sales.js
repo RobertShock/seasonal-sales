f4a09fb147a76c9dfb39220f5811d9e1aa4665f6
Robbie Shock <rob.f.shock@gmail.com>
2017-09-02 13:23:10 -0500

    completed all seasonal-sales

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,67 +1,92 @@
 
-// You work as a developer for a big box store. Here is a JSON representation of a small sample of the products you sell.
-// Here is a JSON representation of some of the departments in your stores.
-function domString(){
+function productsJSONLoad(){
+    let productsData = JSON.parse(this.responseText).products;
+    getCategories(productsData);
 }
 
-
-
-var productsArray = [];
-
-// domString(products);
-
-var productsContainer = document.getElementById("products-container");
-
-// function printProductsArrayToDom(array) {
-//   for ( var i = 0; i < array.length; i++ ) {
-//     var currentProducts = array[i];
-//     var productsDomString = productsDomStringFunction(currentProducts);
-//   productsContainer.innerHTML += productsDomString;
-//   }
-// }
-
-function productsDomStringFunction(currentProducts) {
-  for ( var i = 0; i < currentProducts.length; i++ ) {
-  var productsDomString = "";
-    productsDomString += '<section class="products">';
-    productsDomString +=   '<div class="productsName">';
-    productsDomString +=     '<h2>' + currentProducts[i].name + '</h2>';
-    productsDomString +=   '</div>';
-    productsDomString +=   '<div class="productsPrice">'
-    productsDomString +=     '<p>' + currentProducts[i].price + '</p>';
-    productsDomString +=   '</div>'
-    productsDomString +=   '</section>';
-    writeToDom(productsDomString);
-  };
+function Fail(){
 }
 
-function writeToDom(string){
-  productsContainer.innerHTML = string;
+let theProducts = new XMLHttpRequest();
+theProducts.addEventListener("load", productsJSONLoad);
+theProducts.addEventListener("error", Fail);
+theProducts.open("GET", "products.json");
+theProducts.send();
+
+function getCategories(productsParam){
+    let myCategories = new XMLHttpRequest();
+    myCategories.addEventListener("load", departmentsJSONLoad);
+    myCategories.addEventListener("error", Fail);
+    myCategories.open("GET", "departments.json");
+    myCategories.send();
+
+    function departmentsJSONLoad() {
+        let categoriesData = JSON.parse(this.responseText).categories;
+        combinedArray(productsParam, categoriesData);
+    }
 }
 
-function executeThisCodeAfterFileLoads() {
-  var data = JSON.parse(this.responseText);
-  productsDomStringFunction(data.products);
+let productArrayVariable = [];
+
+function combinedArray(productsArray, categoriesArray){
+    productsArray.forEach(function(product){
+        let currentCategoryId = product["category_id"];
+        categoriesArray.forEach(function(category){
+            if(currentCategoryId === category.id){
+                product["categoryName"] = category.name;
+                product["seasonDiscount"] = category["season_discount"];
+                product["discount"] = category.discount;
+                product["salePrice"] = product.price - product.price * product.discount;
+            }
+        });
+    });
+    productArrayVariable = productsArray;
+    domString(productsArray);
 }
 
-// function executeThisCodeAfterFileLoads2(arrayName){
-//   var data = JSON.parse(this.responseText);
-//     printCategoriesArrayToDom(data.categories);
-// }
+function domString(products){
+    let productString = "";
 
-function executeThisCodeIfFileErrors(){
-  console.log("FAIL TRY AGAIN");
+    for(let i = 0; i < products.length; i++){
+        productString += `<h2>${products[i].name}</h2>`;
+        productString += `<h3>${products[i].categoryName}</h3>`;
+        if(selectedSeason === products[i].seasonDiscount){
+            productString += `<p>${products[i].salePrice.toFixed(2)}</p>`;
+        } else {
+            productString += `<p>${products[i].price}</p>`;
+        }
+    }
+    writeToDom(productString);
 }
 
-var myRequest = new XMLHttpRequest();
-myRequest.addEventListener("load", executeThisCodeAfterFileLoads);
-myRequest.addEventListener("error", executeThisCodeIfFileErrors);
-myRequest.open("GET", "products.json");
-myRequest.send()
+function writeToDom(strang){
+    mainContainer.innerHTML = strang;
+}
 
-// var myRequest2 = new XMLHttpRequest();
-// myRequest2.addEventListener("load", executeThisCodeAfterFileLoads2);
-// myRequest2.addEventListener("error", executeThisCodeIfFileErrors);
-// myRequest2.open("GET", "categories.json");
-// myRequest2.send()
+let selectBtn = document.getElementById("selectBtn");
+let selectedSeason = "";
+
+selectBtn.addEventListener('change', function(e){
+    console.log(e);
+    
+    let selectOptions = e.target.childNodes;
+    for(let i = 0; i < selectOptions.length; i++){
+        if(selectOptions[i].selected) {
+            console.log(selectOptions[i].value);
+            selectedSeason = selectOptions[i].value;
+        }
+    }
+        domString(productArrayVariable);
+    });
+
+
+
+
+
+
+
+
+
+
+
 
